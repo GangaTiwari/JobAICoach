7d2b433b082fe34eecbdf11346fd54ed8852ec96
Ganga Tiwari <tiwariganga0208@gmail.com>
2025-06-20 18:30:25 +0530

    Downgrade React and Made changes in UI

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -52,12 +52,12 @@ export default function Page() {
                     </a>
       
                     <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                      Welcome to AI Interview Moker 🦑
+                      Welcome to JobAICoach
                     </h1>
       
                     <p className="mt-4 leading-relaxed text-gray-500">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+                     Transform your interview skills with AI-powered precision.
+                     Ace every question with personalized mock sessions.
                     </p>
                   </div>
                   <SignIn />
